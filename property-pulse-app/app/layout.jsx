51555fb651e88ac6afce33bffc329a91d559b778
jramos5883 <jramos5883@gmail.com>
2024-03-13 16:01:34 -0700

    Finished setting up layout and homepage

---
+++ property-pulse-app/app/layout.jsx
@@ -0,0 +1,15 @@
+import "@/assests/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+export default function MainLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
